d9d4dac1ca08e797797d1fc7a89ac8d440204c24
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-13 10:26:41 -0300

    ADD Passport

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Las 2 primeras rutas son públicas, y las 2 siguientes requieren de autenticación.
+Route::group([
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+], function () {
+    Route::post(
+        'login',
+        'AuthController@login'
+    );
+    Route::post('signup', 'AuthController@signUp');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get(
+            'user',
+            'AuthController@user'
+        );
+        Route::get(
+            '/task/{id?}',
+            'TaskController@index'
+        );
+        Route::get(
+            '/task/detail/{id}',
+            [TaskController::class, 'show']
+        );
+        Route::post(
+            '/task',
+            [TaskController::class, 'store']
+        );
+        Route::post(
+            '/task/edit/{id}',
+            [TaskController::class, 'update']
+        );
+        Route::post('/task/delete/{id}', function ($id) {
+            return TaskController::delete($id);
+        });
+    });
 });
