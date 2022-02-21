bdfc4d602dc8d6be4404c3bda029fd4efbd7443f
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-21 08:47:17 -0300

    Relación modelos

--- laravel/app/Models/Convenio.php
+++ laravel/app/Models/Convenio.php
@@ -20,4 +20,10 @@ class Convenio extends Model
         'date_end',
         'state',
     ];
+
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -42,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function companys()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function convenios()
+    {
+        return $this->hasMany(Convenio::class);
+    }
 }
