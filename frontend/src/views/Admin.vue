6d8112839562d40020bd720e77a3f303d07cb499
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-02-25 12:57:04 -0300

    Add tables

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -12,30 +12,21 @@
                 :desactived="!navActive"
                 @click="activarDesactivarNav"
               >
-                Active</b-nav-item
+                Empresas</b-nav-item
               >
               <b-nav-item
                 :active="!navActive"
                 :desactived="!navActive"
                 @click="activarDesactivarNav"
-                >Inactive</b-nav-item
+                >Usuarios</b-nav-item
               >
               <!-- <b-nav-item disabled>Disabled</b-nav-item> -->
             </b-nav>
           </b-card-header>
 
-          <b-card-body class="text-center">
-            <b-card-text v-if="navActive">
-              1 With supporting text below as a natural lead-in to additional
-              content.
-            </b-card-text>
-
-            <b-card-text v-else>
-              2 With supporting text below as a natural lead-in to additional
-              content.
-            </b-card-text>
-
-            <b-button variant="primary">Go somewhere</b-button>
+          <b-card-body>
+            <Empresas v-if="navActive" />
+            <Usuarios v-else />
           </b-card-body>
         </b-card>
       </div>
@@ -44,7 +35,13 @@
 </template>
 
 <script>
+import Empresas from "../components/admin/Empresas.vue";
+import Usuarios from "../components/admin/Usuarios.vue";
 export default {
+  components: {
+    Empresas,
+    Usuarios,
+  },
   name: "SgpeAdmin",
 
   data() {
@@ -81,38 +78,5 @@ body {
   max-width: 70%;
   padding: 15px;
   margin: auto;
-}
-
-.form-signin .checkbox {
-  font-weight: 400;
-}
-
-.form-signin .form-floating:focus-within {
-  z-index: 2;
-}
-
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
 }
 </style>
